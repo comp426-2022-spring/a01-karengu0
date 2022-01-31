0c8181f25856555a876bb7e999f76f699c4b64e5
Karen Guo <karengu0@live.unc.edu>
2022-01-30 22:29:33 -0500

    Global store data

--- server.js
+++ server.js
@@ -15,16 +15,17 @@ const port = argument[portArgument] || 3000
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./www/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-
+let data = "";
 // If there is an error, put it on the console error, return, and exit with error code 1. 
 // Do not be nice about exiting.
-fs.readFile('./www/index.html', 'utf8', (err, data) => {
+fs.readFile('./www/index.html', 'utf8', (err, data2) => {
     if (err) {
         console.error(err)
         return
         process.exit(1)
     }
-    console.log(data)
+    console.log(data2)
+    data = data2;
 })
 
 // Define a const `server` as an arrow function using http.createServer. 
